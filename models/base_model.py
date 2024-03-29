2b433820d61bd9497c827c2cd269fe81d33cb8fe
yassineboucham <yassineboucham11@gamil.com>
2024-03-29 18:45:27 +0000

    give it the rolse

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,8 @@ class BaseModel:
             .format(BaseModel.__name__, self.id, self.__dict__)
 
     def save(self):
-        """save the apdate"""
+        """save the apdate
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
