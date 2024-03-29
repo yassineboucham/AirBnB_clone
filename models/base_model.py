8b05a83eaa99e989aabca7193b751d889bb9d9a3
yassineboucham <yassineboucham11@gamil.com>
2024-03-29 22:32:33 +0000

    give it the rolse

--- models/base_model.py
+++ models/base_model.py
@@ -16,23 +16,22 @@ class BaseModel:
     def __init__(self):
         """init"""
         self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+    def save(self):
+        """save the apdate"""
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """to_dict"""
+        dictt = {**self.__dict__}
+        dictt["created_at"] = dictt["created_at"].isoformat()
+        dictt["updated_at"] = dictt["updated_at"].isoformat()
+        dictt["__class__"] = BaseModel.__name__
+        return dictt
 
     def __str__(self):
         """return this format: [<class name>] (<self.id>) <self.__dict__>"""
         return "[{}] ({}) {}"\
             .format(BaseModel.__name__, self.id, self.__dict__)
-
-    def save(self):
-        """save the apdate
-        """
-        self.updated_at = datetime.now()
-
-    def to_dict(self):
-        """to_dict"""
-        dictt = {**self.__dict__}
-        dictt["__class__"] = BaseModel.__name__
-        dictt["created_at"] = dictt["created_at"].isoformat()
-        dictt["updated_at"] = dictt["updated_at"].isoformat()
-        return dictt
