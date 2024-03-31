86828e0b5d690c531150b9c16c9a0df3d315341d
yassineboucham <yassineboucham11@gamil.com>
2024-03-31 10:16:42 +0100

    sum edits

--- models/base_model.py
+++ models/base_model.py
@@ -5,27 +5,36 @@ from datetime import datetime
 
 
 class BaseModel:
-    """defines all common attributes/methods for other classes"""
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self):
-        """init"""
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
 
     def save(self):
-        """save the apdate"""
+        """Update updated_at with the current datetime."""
         self.updated_at = datetime.today()
 
     def to_dict(self):
-        """to_dict"""
-        dictt = self.__dict__.copy()
-        dictt["created_at"] = dictt["created_at"].isoformat()
-        dictt["updated_at"] = dictt["updated_at"].isoformat()
-        dictt["__class__"] = BaseModel.__name__
-        return dictt
+        """Return the dictionary of the BaseModel instance.
+
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
 
     def __str__(self):
-        """return this format: [<class name>] (<self.id>) <self.__dict__>"""
-        name = BaseModel.__name__
-        return "[{}] ({}) {}".format(name, self.id, self.__dict__)
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
