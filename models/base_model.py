b24a3c5198cd2a146a0b937d0aa607c6be5a6b31
yassineboucham <yassineboucham11@gamil.com>
2024-04-01 18:44:28 +0100

    pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,10 @@
 Defines the BaseModel class.
 
 """
+import models
 from uuid import uuid4
 from datetime import datetime
+
 
 class BaseModel:
     """Represents the BaseModel of the HBnB project."""
@@ -23,13 +25,15 @@ class BaseModel:
             dictadd = self.__dict__.copy()
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    dictadd[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    time = "%Y-%m-%dT%H:%M:%S.%f"
+                    dictadd[key] = datetime.strptime(value, time)
                 else:
                     dictadd[key] = value
 
     def save(self):
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Return the dictionary of the BaseModel instance.
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,13 +9,13 @@ import json
 
 
 class FileStorage:
-    
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         return self.__objects
-    
+
     def new(self, obj):
         self.__objects["<obj class name>.id"] = obj
 
